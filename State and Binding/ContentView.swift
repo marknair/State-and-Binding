abe765ed19dc5981636d64d61599f7c1d6be2798
Mark Nair <marknair@me.com>
2023-11-11 16:27:55 -0600

    Initial Commit

---
+++ State and Binding/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  State and Binding
+//
+//  Created by Mark Nair on 11/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ State and Binding/State_and_BindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  State_and_BindingApp.swift
+//  State and Binding
+//
+//  Created by Mark Nair on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct State_and_BindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
